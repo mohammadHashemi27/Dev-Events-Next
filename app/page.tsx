2cc3208fafb42c5e91c7df5b49ad3c71b244dad4
mohmmadHashemi <s.mohammad.2w7@gmail.com>
2026-02-22 14:05:13 +0330

    reEdit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
-
+import { IEvent } from "@/database";
 import { cacheLife } from "next/cache";
-import { events } from "@/lib/constants";
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
@@ -28,7 +27,7 @@ const Page = async () => {
         <ul className="events">
           {events &&
             events.length > 0 &&
-            events.map((event) => (
+            events.map((event: IEvent) => (
               <li key={event.title} className="list-none">
                 <EventCard {...event} />
               </li>
